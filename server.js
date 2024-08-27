d1c71a66e3ddd57ac29a781a3f49599ba83dd178
Sandra-0701 <sandrasindhu0701@gmail.com>
2024-08-27 23:40:21 +0530

    added

--- server.js
+++ server.js
@@ -8,10 +8,11 @@ const port = process.env.PORT || 5000;  // Use Vercel's port or fallback to 5000
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: 'https://web-page-fetcher.vercel.app/', // Allow only your frontend domain
+  origin: true,
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
+
 
 
 // Route imports
