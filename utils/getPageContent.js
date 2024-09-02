e24067ddee5c51548c660aef33c49bb024ec97aa
Sandra-0701 <sandrasindhu0701@gmail.com>
2024-09-02 12:10:30 +0530

    added

--- utils/getPageContent.js
+++ utils/getPageContent.js
@@ -1,3 +1,4 @@
+//utils/getPageContent.js
 const axios = require('axios');
 const cheerio = require('cheerio');
 
@@ -11,7 +12,7 @@ const getPageContent = async (url, onlyUhf = false) => {
     const footer = $('footer').html() || '';
 
     // Extract main content if not onlyUhf
-    const content = onlyUhf ? '' : $('main.microsoft-template-layout-container').html() || $('section#primaryArea[role="main"]').html() || '';
+    const content = onlyUhf ? '' : $('main.microsoft-template-layout-container').html() || '';
 
     // Extract meta tags or other page properties
     const pageProperties = $('meta').map((_, meta) => ({
